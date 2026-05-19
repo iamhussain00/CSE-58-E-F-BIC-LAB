68caa9fe4e92ef5f247fe1b3fc31129867c31ebb
ali hussain <140701227+iamhussain00@users.noreply.github.com>
2026-05-19 16:02:13 +0600

    Add multiplication check for numbers greater than 50

---
+++ Task 1/YouDo3.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first Number :"))
+b = int(input("Enter second Number :"))
+
+
+mul = a * b
+
+is_greater = mul > 50
+
+print(f"multiplication of Given number is greater then 50: {is_greater}")
+
