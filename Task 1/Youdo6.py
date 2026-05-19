1071ff8a8b17182f6acdce88db50ce84161e8736
ali hussain <140701227+iamhussain00@users.noreply.github.com>
2026-05-19 16:11:46 +0600

    Add temperature evaluation script

---
+++ Task 1/Youdo6.py
@@ -0,0 +1,8 @@
+temparature = int(input("Enter the temparature: "))
+
+if temparature > 30:
+    print("It is hot")
+elif temparature <= 30 and temparature >= 20:
+    print("It is Nice")
+else:
+    print("It is cold")
