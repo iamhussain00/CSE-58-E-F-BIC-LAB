c2a005edecdbedc897bbcbd7f55d67d1562e174a
ali hussain <140701227+iamhussain00@users.noreply.github.com>
2026-06-09 15:54:25 +0600

    Add function to generate reverse complement of DNA
    Implement DNA complement and reverse functionality.

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,15 @@
 
+st = input()
+s = ""
+for i in range (0,len(st)):
+    if(st[i] == 'A'):
+        s += 'T'
+    elif(st[i] == 'C'):
+        s += "G"
+    elif(st[i] == 'G'):
+        s += 'C'
+    else:
+        s += 'A'
+s = s[::-1]
+
+print(s)
