f5d6a5ee5a2c5980ab60f6113970d1591ad9460d
ali hussain <140701227+iamhussain00@users.noreply.github.com>
2026-06-09 15:55:01 +0600

    Implement substring search in Problem4.py
    Add substring search logic to find occurrences of 'st' in 's'.

--- Task 2/Problem4.py
+++ Task 2/Problem4.py
@@ -1,1 +1,12 @@
-
+st = input()
+s = input()
+for i in range (0,len(s) - len(st)):
+    flag = 0
+    for j in range(0,len(st)):
+       if(st[j] != s[i + j]):
+           flag = 1
+           break
+       
+    if(flag == 0):
+        print(i,end =" ")
+           
