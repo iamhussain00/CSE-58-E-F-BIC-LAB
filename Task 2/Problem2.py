787025f6b833b6f4dcc8027dd1cff921848a87d4
ali hussain <140701227+iamhussain00@users.noreply.github.com>
2026-06-09 16:20:34 +0600

    Implement substring frequency counter
    Count occurrences of k-length substrings in input.

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,21 @@
 
+st = input().strip()
+k = int(input())
+
+count = {}
+
+for i in range(len(st) - k + 1):
+    pattern = st[i:i+k]
+
+    if pattern in count:
+        count[pattern] += 1
+    else:
+        count[pattern] = 1
+
+
+max_count = max(count.values())
+
+
+for pattern in count:
+    if count[pattern] == max_count:
+        print(pattern, end=" ")
