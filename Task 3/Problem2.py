a61c863d541be00c11de2328f173d01e74c340ff
ali hussain <140701227+iamhussain00@users.noreply.github.com>
2026-06-16 15:53:37 +0600

    Implement string comparison with mismatch tolerance
    Add functionality to compare strings with a tolerance for mismatches.

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,12 @@
+s = input()
+a = input()
+k = int(input())
 
+for i in range(len(a) - len(s)):
+    cnt = 0
+    for j in range(len(s)):
+        if(s[j] != a[i + j]):
+            cnt += 1
+            
+    if(cnt <= k):
+        print(i, end = " ")
